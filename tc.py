20e04dcfaed7ae5b97f5b5dce8a09369bd563f52
Leonard Huang <leihuang.org@gmail.com>
2018-07-08 17:07:10 +0800

    Update and rename hytc.py to tc.py

--- tc.py
+++ tc.py
@@ -8,12 +8,13 @@ import linecache
 logging.basicConfig(format='%(asctime)s [line:%(lineno)d] %(levelname)s %(message)s',
                     datefmt='%D %H:%M:%S', filename='/root/tc.log', level=logging.INFO)
 
-server_limit_bandwidth = 20.00
-user_bandwidth = '6.5'
-device = 'eth1'
-white_port_list = [':20431']
-temp_file_path = '/root/hytc_temp.txt'
-temp_count = 1
+server_limit_bandwidth = 150.00
+user_bandwidth = '30'
+device = 'ens192'
+white_port_list = []  #[':443', ':80']
+temp_file_path = '/root/tc_data.txt'
+current_count = 1
+max_count = 500
 
 command = '/usr/local/iftop/sbin/iftop -i ' + device + ' -N -n -B -P -s 2 -L 2 -o 2s -t > ' + temp_file_path
 tc_command = 'tcset --add --device ' + device + ' --direction outgoing --rate ' + user_bandwidth + 'M --network '
@@ -28,7 +29,9 @@ def verify_param(user_ip, user_traffic, total_traffic):
         return False
     if 'KB' not in user_traffic and 'MB' not in user_traffic:
         return False
-    if 'KB' in user_traffic and float(user_traffic.strip('KB')) < 900:
+    # if 'KB' in user_traffic and float(user_traffic.strip('KB')) < 900:
+    #    return False
+    if 'KB' in user_traffic:
         return False
     return True
 
@@ -56,19 +59,24 @@ def get_param(file_path, num):
 def run_analyze(num):
     user_traffic_line, user_ip_line, user_ip, user_max_traffic, total_traffic = get_param(temp_file_path, num)
     logging.info('num:%d, ip:%s, mx:%s, total:%s', num, user_ip, user_max_traffic, total_traffic)
-    if white_port not in user_traffic_line and white_port not in user_ip_line:
-        analyze_traffic(user_ip, user_max_traffic, total_traffic)
+    is_white_port = false
+    for white_port in white_port_list:
+      if white_port in user_traffic_line or white_port in user_ip_line:
+        is_white_port = true
+    
+    if not is_white_port:
+      analyze_traffic(user_ip, user_max_traffic, total_traffic)
 
 
 if __name__ == "__main__":
     while True:
-        logging.info('cycle [%d] times', temp_count)
-        if temp_count == 1000:
+        logging.info('cycle [%d] times', current_count)
+        if current_count == max_count:
             logging.info('clean tc..')
-            temp_count = 1
+            current_count = 1
             os.system(clean_tc_command)
         os.system(command)
         run_analyze(1)
         run_analyze(2)
-        temp_count += 1
+        current_count += 1
         time.sleep(5)
